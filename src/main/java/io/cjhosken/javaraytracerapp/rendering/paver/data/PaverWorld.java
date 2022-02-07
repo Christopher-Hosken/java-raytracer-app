a0292fc351e6df68576733cfe4c050648b545b59
Christopher Hosken <hoskenchristopher@gmail.com>
2022-02-07 14:30:20 +0700

    Create PaverWorld.java

---
+++ src/main/java/io/cjhosken/javaraytracerapp/rendering/paver/data/PaverWorld.java
@@ -0,0 +1,30 @@
+public class PaverWorld {
+  PaverObject[] objects;
+  PaverCamera camera;
+  
+  public PaverWorld() {}
+  
+  public PaverCamera camera() {
+    return camera;
+  }
+  
+  public PaverObject[] objects() {
+    return objects;
+  }
+  
+  public void setCamera(PaverCamera camera) {
+     this.camera = camera;
+  }
+  
+  public void setObjects(PaverObject[] objects) {
+    this.objects = objects;
+  }
+  
+  public void objectsFromJRSObjects(JRSObject[] jrsObjects) {
+    objects = new PaverObject[jrsObjects.length];
+    
+    for (int idx = 0; idx < objects.length; idx++) {
+      objects[idx] = PaverObject.fromJRSObject(jrsObjects[idx]);
+    }
+  }
+}
