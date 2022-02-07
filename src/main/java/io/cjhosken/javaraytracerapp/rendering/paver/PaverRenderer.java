62bff79357ed231b28569ed4e29bd85d0c96190f
Christopher Hosken <hoskenchristopher@gmail.com>
2022-02-07 14:23:21 +0700

    Update PaverRenderer.java

--- src/main/java/io/cjhosken/javaraytracerapp/rendering/paver/PaverRenderer.java
+++ src/main/java/io/cjhosken/javaraytracerapp/rendering/paver/PaverRenderer.java
@@ -1,1 +1,18 @@
 
+public class PaverRenderer {
+  public PaverRenderer() {}
+  
+  public static BufferedImage renderJRS(JRSFile jrs) throws IOException {
+    JRSSettings settings = jrs.settings();
+    
+    BufferedImage image = new BufferedImage(settings.width(), settings.height(), BufferedImage.TYPE_INT_RGB);
+   
+    PaverWorld world = jrs.world().toPaverWorld();
+    
+    PaverCamera camera = world.camera();
+    
+    image = camera.render(image, world, settings.samples(), settings.bounces());
+    
+    return image;
+  }
+}
