f6a925ed3d5be5e5c53dfd236ddbe8a07936b405
Christopher Hosken <hoskenchristopher@gmail.com>
2022-02-07 14:07:01 +0700

    Update JRSObject.java

--- src/main/java/io/cjhosken/javaraytracerapp/jrs/datatypes/JRSObject.java
+++ src/main/java/io/cjhosken/javaraytracerapp/jrs/datatypes/JRSObject.java
@@ -10,9 +10,90 @@ public class JRSObject {
     protected Vector3d location = new Vector3d();
     protected Vector3d rotation = new Vector3d();
     protected Vector3d scale = new Vector3d(1,1,1);
-    protected JRSMesh meshData = new JRSMesh();
+    protected JRSMesh mesh = new JRSMesh();
 
     protected JRSShader shader = new JRSShader();
+    
+    public JRSObject(String name, JRSObjectType type) {
+        this.name = name;
+        this.type = type;
+    }
+    
+    public String name() {
+        return name;
+    }
+    
+    public JRSObjectType type() {
+        return type;
+    }
+    
+    public Vector3d location() {
+        return location;
+    }
+    
+    public Vector3d rotation() {
+        return rotation;
+    }
+    
+    public Vector3d scale() {
+        return scale;
+    }
+    
+    public JRSMesh mesh() {
+        return mesh;
+    }
+    
+    public TriangleMesh mesh() {
+        TriangleMesh triMesh = new TriangleMesh();
+        
+        return triMesh;
+    }
+    
+    public JRSShader shader() {
+        return shader;
+    }
+    
+    public FX3DShader shader() {
+        FX3DShader fx3dShader = new FX3DShader();
+        
+        return fx3dShader;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public void setType(JRSObjectType type) {
+        this.type = type;
+    }
+    
+    public void setLocation(Vector3d location) {
+        this.location = location;
+    }
+    
+    public void setRotation(Vector3d rotation) {
+        this.rotation = rotation;
+    }
+    
+    public void setScale(Vector3d scale) {
+        this.scale = scale;
+    }
+    
+    public void setMesh(JRSMesh mesh) {
+        this.mesh = mesh
+    }
+    
+    public void setMesh(TriangleMesh mesh) {
+    
+    }
+    
+    public void setShader(JRSShader shader) {
+        this.shader= shader;
+    }
+    
+    public void setShader(FX3DShader shader) {
+    
+    }
 
     public JSONObject toJSON() {
         JSONObject object = new JSONObject();
@@ -23,7 +104,7 @@ public class JRSObject {
         object.put("scale", scale.toJSONArray());
 
         if (type == JRSObjectType.OBJ) {
-            object.put("mesh", meshData.toJSON());
+            object.put("mesh", mesh.toJSON());
         }
 
         object.put("shader", shader.toJSON());
@@ -39,7 +120,7 @@ public class JRSObject {
         location.fromJSONArray(jrs.getJSONArray("scale"));
 
         if (type == JRSObjectType.OBJ) {
-            meshData.fromJSON(jrs.getJSONObject("mesh"));
+            mesh.fromJSON(jrs.getJSONObject("mesh"));
         }
 
         shader.fromJSON(jrs.getJSONObject("shader"));
