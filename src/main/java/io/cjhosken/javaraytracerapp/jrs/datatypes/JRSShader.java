de7257166ff143b5f69b3012377c67014d7a84bf
Christopher Hosken <hoskenchristopher@gmail.com>
2022-02-07 14:10:18 +0700

    Update JRSShader.java

--- src/main/java/io/cjhosken/javaraytracerapp/jrs/datatypes/JRSShader.java
+++ src/main/java/io/cjhosken/javaraytracerapp/jrs/datatypes/JRSShader.java
@@ -9,6 +9,40 @@ public class JRSShader {
     private Vector3d color = new Vector3d(1, 0, 1);
     private double specular = 1;
     private double roughness = 0.5;
+    
+    public JRSShader() {}
+    
+    public String name() {
+        return name;
+    }
+    
+    public Vector3d color() {
+        return color;
+    }
+    
+    public double specular() {
+        return specular;
+    }
+    
+    public double roughness() {
+        return roughness;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public void setColor(Vector3d color) {
+        this.color = color;
+    }
+    
+    public void setSpecular(double specular) {
+        this.specular = specular;
+    }
+    
+    public void setRoughness(double roughness) {
+        this.roughness = roughness;
+    }
 
     public void fromJSON(JSONObject jrs) {
         name = jrs.getString("name");
