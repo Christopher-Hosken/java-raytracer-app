eded69395d52777dd75482b66c2ec245f80c5c76
Christopher Hosken <hoskenchristopher@gmail.com>
2022-02-07 14:34:04 +0700

    Update PaverObject.java

--- src/main/java/io/cjhosken/javaraytracerapp/rendering/paver/data/PaverObject.java
+++ src/main/java/io/cjhosken/javaraytracerapp/rendering/paver/data/PaverObject.java
@@ -1,9 +1,20 @@
 public class PaverObject {
+  protected String name;
+  protected Vector3d location;
+  protected Vector3d rotation;
+  protected Vector3d scale;
+  protected Vector3d tmpNormal;
+  protected double tmpT;
+  protected PaverShader shader = new PaverDiffuse();
+  
   public PaverObject() {}
   
   
-  public static PaverObject fromJRS() {
+  public static PaverObject fromJRS(JRSObject object) {
     return new PaverObject();
   }
   
+  public double intersect(Ray ray) {
+    return -1;
+  }
 }
